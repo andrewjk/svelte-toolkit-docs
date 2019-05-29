713f5c5c94eaebcd823afc5c92d49233538bad0a
Andrew K <andrewjk@gmail.com>
2019-05-29 16:50:47 +1000

    Added some more icons and example code

--- src/routes/components/icons.svelte
+++ src/routes/components/icons.svelte
@@ -1,43 +1,65 @@
 <script>
-  import { ArrowUp, ArrowDown, ArrowLeft, ArrowRight, Cross } from "../../../../svelte-toolkit/main.js";
+  import {
+    ArrowUp,
+    ArrowDown,
+    ArrowLeft,
+    ArrowRight,
+    ChevronUp,
+    ChevronDown,
+    ChevronLeft,
+    ChevronRight,
+    Bars,
+    Cross
+  } from "../../../../svelte-toolkit/main.js";
+
+  function exampleCode() {
+    return (
+      "<ArrowUp />\n" +
+      "<ArrowDown />\n" +
+      "<ArrowLeft />\n" +
+      "<ArrowRight />\n" +
+      "<ChevronUp />\n" +
+      "<ChevronDown />\n" +
+      "<ChevronLeft />\n" +
+      "<ChevronRight />\n" +
+      "<Bars />\n" +
+      "<Cross />"
+    );
+  }
 </script>
 
 <div class="container">
   <h2>Icons</h2>
   <p>
-    Some basic SVG icons made while I figure out how to incorporate FontAwesome and other icon sets.
+    Some basic SVG icons made while I figure out how to incorporate FontAwesome
+    and other icon sets.
   </p>
 
   <h3>Demo</h3>
   <div class="block">
-    <ArrowUp/>
-    <ArrowDown/>
-    <ArrowLeft/>
-    <ArrowRight/>
-    <Cross/>
-  </div>
-
-  <h3>Properties</h3>
-  <div class="block">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Default</th>
-          <th>Description</th>
-          <th>Change</th>
-        </tr>
-      </thead>
-      <tbody>
-
-      </tbody>
-    </table>
+    <h4>Arrows</h4>
+    <div class="block">
+      <ArrowUp />
+      <ArrowDown />
+      <ArrowLeft />
+      <ArrowRight />
+    </div>
+    <h4>Chevrons</h4>
+    <div class="block">
+      <ChevronUp />
+      <ChevronDown />
+      <ChevronLeft />
+      <ChevronRight />
+    </div>
+    <h4>Misc</h4>
+    <div class="block">
+      <Bars />
+      <Cross />
+    </div>
   </div>
 
   <h3>Code</h3>
   <div class="block">
-    <pre>
-TODO:
-    </pre>
+    <pre>{exampleCode()}</pre>
   </div>
 </div>
