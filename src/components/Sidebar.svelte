a67c5f2e1516c775e1dc2e3bd924607402341511
Andrew K <andrewjk@gmail.com>
2019-06-27 13:24:58 +1000

    Is this how I do relative paths?

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -9,100 +9,100 @@
 <SubNavMenuHeader>General</SubNavMenuHeader>
 <SubNavMenu type="vertical">
   <SubNavMenuItem>
-    <a href="/general/base">Base styles</a>
+    <a href="general/base">Base styles</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/general/layout">Layout styles</a>
+    <a href="general/layout">Layout styles</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/general/grid">Grid</a>
+    <a href="general/grid">Grid</a>
   </SubNavMenuItem>
 </SubNavMenu>
 
 <SubNavMenuHeader>Components</SubNavMenuHeader>
 <SubNavMenu type="vertical">
   <SubNavMenuItem>
-    <a href="/components/accordion">Accordion</a>
+    <a href="components/accordion">Accordion</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/alert">Alert</a>
+    <a href="components/alert">Alert</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/badge">Badge</a>
+    <a href="components/badge">Badge</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/breadcrumb">Breadcrumb</a>
+    <a href="components/breadcrumb">Breadcrumb</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/button">Button</a>
+    <a href="components/button">Button</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/calendar">Calendar</a>
+    <a href="components/calendar">Calendar</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/navbar">Nav Bar</a>
+    <a href="components/navbar">Nav Bar</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/notification">Notification</a>
+    <a href="components/notification">Notification</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/offscreen">Off-Screen</a>
+    <a href="components/offscreen">Off-Screen</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/pagination">Pagination</a>
+    <a href="components/pagination">Pagination</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/panel">Panel</a>
+    <a href="components/panel">Panel</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/progress">Progress</a>
+    <a href="components/progress">Progress</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/subnavmenu">Sub Nav Menu</a>
+    <a href="components/subnavmenu">Sub Nav Menu</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/switcher">Switcher</a>
+    <a href="components/switcher">Switcher</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/tabgroup">Tab Group</a>
+    <a href="components/tabgroup">Tab Group</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/components/wizardnav">Wizard Navigation</a>
+    <a href="components/wizardnav">Wizard Navigation</a>
   </SubNavMenuItem>
 </SubNavMenu>
 
 <SubNavMenuHeader>Forms</SubNavMenuHeader>
 <SubNavMenu type="vertical">
   <SubNavMenuItem>
-    <a href="/forms/field">Field</a>
+    <a href="forms/field">Field</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/forms/checkbox">CheckBox</a>
+    <a href="forms/checkbox">CheckBox</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/forms/textbox">TextBox</a>
+    <a href="forms/textbox">TextBox</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/forms/validation">Validation</a>
+    <a href="forms/validation">Validation</a>
   </SubNavMenuItem>
 </SubNavMenu>
 
 <SubNavMenuHeader>Dialogs</SubNavMenuHeader>
 <SubNavMenu type="vertical">
   <SubNavMenuItem>
-    <a href="/dialogs/info">Info Dialog</a>
+    <a href="dialogs/info">Info Dialog</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/dialogs/confirm">Confirm Dialog</a>
+    <a href="dialogs/confirm">Confirm Dialog</a>
   </SubNavMenuItem>
   <SubNavMenuItem>
-    <a href="/dialogs/prompt">Prompt Dialog</a>
+    <a href="dialogs/prompt">Prompt Dialog</a>
   </SubNavMenuItem>
 </SubNavMenu>
 
 <SubNavMenuHeader>Icons</SubNavMenuHeader>
 <SubNavMenu type="vertical">
   <SubNavMenuItem>
-    <a href="/components/icons">Icons</a>
+    <a href="components/icons">Icons</a>
   </SubNavMenuItem>
 </SubNavMenu>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -8,6 +8,7 @@
 
 <svelte:head>
   <title>Svelte Toolkit</title>
+  <base href="/svelte-toolkit-docs/">
 </svelte:head>
 
 <div class="page">
