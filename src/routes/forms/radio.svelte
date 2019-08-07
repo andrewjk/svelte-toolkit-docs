260c028817cbd617a33405325dfdfa9c11fcf184
Andrew K <andrewjk@gmail.com>
2019-08-07 18:55:45 +1000

    Added a Radio form component

---
+++ src/routes/forms/radio.svelte
@@ -0,0 +1,111 @@
+<script>
+  import { Radio } from "../../../../svelte-toolkit";
+
+  let type = "";
+  let name = "field1";
+  let label = "Crocodile";
+  let group = "";
+  let value = "Crocodile";
+
+  import { items } from "../../stores/items";
+
+  $: exampleCode = `
+import { Radio } from "svelte-toolkit";
+
+<Radio type="${type}" name="${name}" label="${label}" value="${value}" bind:group />`.trim();
+</script>
+
+<svelte:head>
+  <title>Radio | Svelte Toolkit</title>
+</svelte:head>
+
+<div class="container">
+  <h2>Radio</h2>
+  <p>A radio button for selecting a single value from a group.</p>
+
+  <h3>Demo</h3>
+  <div class="block">
+    <Radio {type} {name} {label} {value} bind:group />
+    {#each $items as item}
+      <Radio
+        {type}
+        {name}
+        label={item.name}
+        value={item.name}
+        bind:group />
+    {/each}
+  </div>
+
+  <h3>Properties</h3>
+  <div class="block">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Default</th>
+          <th>Description</th>
+          <th>Change</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>name</td>
+          <td />
+          <td>The name of the group field selected with the radio</td>
+          <td>
+            <input type="text" bind:value={name} />
+          </td>
+        </tr>
+        <tr>
+          <td>type</td>
+          <td />
+          <td>info, success, warning, or danger</td>
+          <td>
+            <select bind:value={type}>
+              <option>-</option>
+              <option>info</option>
+              <option>success</option>
+              <option>warning</option>
+              <option>danger</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>group</td>
+          <td />
+          <td>The value of the group field selected with the radio</td>
+          <td>
+            <select bind:value={group}>
+              <option>-</option>
+              <option>{value}</option>
+              {#each $items as item}
+                <option>{item.name}</option>
+              {/each}
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>value</td>
+          <td />
+          <td>The group value to set when this radio is selected</td>
+          <td>
+            <input type="text" bind:value />
+          </td>
+        </tr>
+        <tr>
+          <td>label</td>
+          <td />
+          <td>The text label to display next to the radio</td>
+          <td>
+            <input type="text" bind:value={label} />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <h3>Code</h3>
+  <div class="block">
+    <pre>{exampleCode}</pre>
+  </div>
+</div>
