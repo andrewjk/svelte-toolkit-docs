bed228fbd65e07b9f17dc814043daa3725e6197e
Andrew K <andrewjk@gmail.com>
2019-08-15 12:35:46 +1000

    Messed around with sizing and spacing - font is now a little smaller

--- src/routes/general/base.svelte
+++ src/routes/general/base.svelte
@@ -3,20 +3,26 @@
 </svelte:head>
 
 <div class="container">
-    <h1>Base</h1>
+  <h1>Base</h1>
 
-    <h2>Typography</h2>
-    
-    <p>Font sizes and spacing are set in rems, so that you can easily adjust sizing by setting html font-size. Base rem sizing has been adjusted by default so that 1rem = 10px.</p>
+  <h2>Typography</h2>
 
-    <p>Text is set in sans-serif at 1.5rem (15px) with a line-height of 1.4.</p>
+  <p>
+    Font sizes and spacing are set in rems, so that you can easily adjust sizing
+    by setting html font-size. Base rem sizing has been adjusted by default so
+    that 1rem = 10px.
+  </p>
 
-    <p>Headings are set at the following sizes by default:</p>
+  <p>Text is set in sans-serif at 1.4rem (14px) with a line-height of 1.4.</p>
 
-    <h1>Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
-    <h4>Heading 4</h4>
-    <h5>Heading 5</h5>
-    <h6>Heading 6</h6>
+  <h2>Headings</h2>
+
+  <p>Headings are set with the following sizes and spacing:</p>
+
+  <h1>Heading 1</h1>
+  <h2>Heading 2</h2>
+  <h3>Heading 3</h3>
+  <h4>Heading 4</h4>
+  <h5>Heading 5</h5>
+  <h6>Heading 6</h6>
 </div>
