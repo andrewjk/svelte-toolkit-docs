4153ad3fbafb79e4cb883649992fd2916580c220
Andrew K <andrewjk@gmail.com>
2019-11-08 18:33:26 +1100

    Feat: allow positioning the drop down menu

--- src/routes/components/dropdown.svelte
+++ src/routes/components/dropdown.svelte
@@ -9,11 +9,13 @@
   let buttonType = "info";
   let buttonSize = "medium";
   let buttonImage = false;
+  let position = "below";
+  let alignment = "left";
 
   $: exampleCode = `
 import { DropDown, DropDownMenuItem } from "svelte-toolkit";
 
-<DropDown buttonType="${buttonType}" buttonSize="${buttonSize}" buttonImage={${buttonImage}}>
+<DropDown buttonType="${buttonType}" buttonSize="${buttonSize}" buttonImage={${buttonImage}} position="${position}" alignment="${alignment}">
   <span>Items...</span>
   <div slot="menu">
     {#each items as item}
@@ -35,7 +37,7 @@ import { DropDown, DropDownMenuItem } from "svelte-toolkit";
 
   <h2>Demo</h2>
   <div class="block">
-    <DropDown {buttonType} {buttonSize} {buttonImage}>
+    <DropDown {buttonType} {buttonSize} {buttonImage} {position} {alignment}>
       {#if buttonImage}
         <ChevronDown />
       {:else}
@@ -103,6 +105,29 @@ import { DropDown, DropDownMenuItem } from "svelte-toolkit";
               <input type="checkbox" bind:checked={buttonImage} />
               Make an image button
             </label>
+          </td>
+        </tr>
+        <tr>
+          <td>position</td>
+          <td>below</td>
+          <td>below, above</td>
+          <td>
+            <select bind:value={position}>
+              <option>below</option>
+              <option>above</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>alignment</td>
+          <td>left</td>
+          <td>left, center, right</td>
+          <td>
+            <select bind:value={alignment}>
+              <option>left</option>
+              <option>center</option>
+              <option>right</option>
+            </select>
           </td>
         </tr>
       </tbody>
