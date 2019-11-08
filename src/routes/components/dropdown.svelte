d4c56a9054c03b8989a90a2b6013e3f7220ed451
Andrew K <andrewjk@gmail.com>
2019-11-08 14:19:09 +1100

    Feat: allow creating a dropdown with an image button

--- src/routes/components/dropdown.svelte
+++ src/routes/components/dropdown.svelte
@@ -1,14 +1,20 @@
 <script>
-  import { DropDown, DropDownMenuItem } from "svelte-toolkit";
+  import {
+    DropDown,
+    DropDownMenuItem,
+    ChevronDown
+  } from "svelte-toolkit";
   import { items } from "../../stores/items";
 
-  let type = "info";
+  let buttonType = "info";
+  let buttonSize = "medium";
+  let buttonImage = false;
 
   $: exampleCode = `
 import { DropDown, DropDownMenuItem } from "svelte-toolkit";
 
-<DropDown type="${type}">
-  Items...
+<DropDown buttonType="${buttonType}" buttonSize="${buttonSize}" buttonImage={${buttonImage}}>
+  <span>Items...</span>
   <div slot="menu">
     {#each items as item}
       <DropDownMenuItem>
@@ -25,12 +31,16 @@ import { DropDown, DropDownMenuItem } from "svelte-toolkit";
 
 <div class="container">
   <h1>Drop Down</h1>
-  <p>A select.</p>
+  <p>A drop-down menu for selecting.</p>
 
   <h2>Demo</h2>
   <div class="block">
-    <DropDown {type}>
-      Items...
+    <DropDown {buttonType} {buttonSize} {buttonImage}>
+      {#if buttonImage}
+        <ChevronDown />
+      {:else}
+        <span>Items...</span>
+      {/if}
       <div slot="menu">
         {#each $items as item}
           <DropDownMenuItem>
@@ -54,16 +64,45 @@ import { DropDown, DropDownMenuItem } from "svelte-toolkit";
       </thead>
       <tbody>
         <tr>
-          <td>type</td>
+          <td>buttonType</td>
           <td>info</td>
           <td>info, success, warning, or danger</td>
           <td>
-            <select bind:value={type}>
+            <select bind:value={buttonType}>
+              <option>-</option>
               <option>info</option>
               <option>success</option>
               <option>warning</option>
               <option>danger</option>
             </select>
+          </td>
+        </tr>
+        <tr>
+          <td>buttonSize</td>
+          <td>medium</td>
+          <td>small, medium, large, or inline</td>
+          <td>
+            <select bind:value={buttonSize}>
+              <option>-</option>
+              <option>small</option>
+              <option>medium</option>
+              <option>large</option>
+              <option>inline</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>buttonImage</td>
+          <td>false</td>
+          <td>
+            Set to true to make this a button that contains an image only. The
+            image must have the class name `icon` for styling.
+          </td>
+          <td>
+            <label>
+              <input type="checkbox" bind:checked={buttonImage} />
+              Make an image button
+            </label>
           </td>
         </tr>
       </tbody>
