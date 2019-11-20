b7350b19ebca7ee859f85d0940bf5fdc24587969
Andrew K <andrewjk@gmail.com>
2019-11-20 17:55:29 +1100

    Feat: added a Card component

--- src/routes/components/card.svelte
+++ src/routes/components/card.svelte
@@ -1,14 +1,17 @@
+<script>
+  import { Card } from "svelte-toolkit";
+</script>
 
 <svelte:head>
   <title>Card | Svelte Toolkit</title>
 </svelte:head>
 
 <div class="container">
-    <h1>Card</h1>
+  <h1>Card</h1>
 
-    <p>The card can be used to stack items into a feed.</p>
+  <p>The card can be used to stack items into a feed.</p>
 
-    <div class="card">This is the first card</div>
-    <div class="card">This is a second card</div>
-    <div class="card">This is yet another card</div>
+  <Card>This is the first card</Card>
+  <Card>This is a second card</Card>
+  <Card>This is yet another card</Card>
 </div>
