9ddccf2f3eaad496462c79af1afe79eac1d35f28
Andrew K <andrewjk@gmail.com>
2019-11-22 18:37:08 +1100

    Feat: search in the sidebar

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -6,6 +6,68 @@
     SubNavMenuItem,
     SubNavMenuHeader
   } from "svelte-toolkit";
+
+  let search = null;
+
+  let pages = [
+    { group: "General", title: "Installation" },
+    { group: "General", title: "Styles" },
+    { group: "General", title: "Theming" },
+    { group: "General", title: "Theme Properties" },
+    { group: "Layout", title: "Page" },
+    { group: "Layout", title: "Container" },
+    { group: "Layout", title: "Grid" },
+    { group: "Layout", title: "Level" },
+    { group: "Layout", title: "Panel" },
+    { group: "Components", title: "Accordion" },
+    { group: "Components", title: "Alert" },
+    { group: "Components", title: "Auto Complete" },
+    { group: "Components", title: "Badge" },
+    { group: "Components", title: "Breadcrumb" },
+    { group: "Components", title: "Button" },
+    { group: "Components", title: "Calendar" },
+    { group: "Components", title: "Card" },
+    { group: "Components", title: "Color Picker" },
+    { group: "Components", title: "Cover Image" },
+    { group: "Components", title: "Date Picker" },
+    { group: "Components", title: "Drop Down" },
+    { group: "Components", title: "Image Button" },
+    { group: "Components", title: "Notification" },
+    { group: "Components", title: "Off Screen" },
+    { group: "Components", title: "Palette" },
+    { group: "Components", title: "Progress" },
+    { group: "Components", title: "Slider" },
+    { group: "Components", title: "Tab Group" },
+    { group: "Components", title: "Table" },
+    { group: "Components", title: "Tag" },
+    { group: "Components", title: "Tag Input" },
+    { group: "Nav", title: "Nav Bar" },
+    { group: "Nav", title: "Sub Nav Menu" },
+    { group: "Nav", title: "Switcher" },
+    { group: "Nav", title: "Pagination" },
+    { group: "Nav", title: "Dot Navigation" },
+    { group: "Nav", title: "Story Navigation" },
+    { group: "Nav", title: "Wizard Navigation" },
+    { group: "Forms", title: "Field" },
+    { group: "Forms", title: "CheckBox" },
+    { group: "Forms", title: "File" },
+    { group: "Forms", title: "Number" },
+    { group: "Forms", title: "Radio" },
+    { group: "Forms", title: "Select" },
+    { group: "Forms", title: "TextBox" },
+    { group: "Forms", title: "TextArea" },
+    { group: "Forms", title: "Validation" },
+    { group: "Dialogs", title: "Info Dialog" },
+    { group: "Dialogs", title: "Confirm Dialog" },
+    { group: "Dialogs", title: "Prompt Dialog" },
+    { group: "Charts", title: "Bar Chart" },
+    { group: "Charts", title: "Line Chart" },
+    { group: "Charts", title: "Pie Chart" },
+    { group: "Icons", title: "Icons" }
+  ];
+
+  let groups = pages.map(p => p.group).filter((element, index, self) => index == self.indexOf(element));
+
 </script>
 
 <style>
@@ -15,193 +77,36 @@
   }
 </style>
 
-<Accordion>
-  <AccordionItem header="GENERAL">
-    <SubNavMenu type="vertical">
+<div class="block">
+  <input type="text" bind:value={search} placeholder="Search..." />
+</div>
+{#if search}
+  <SubNavMenu type="vertical">
+    {#each pages.filter(p =>
+      p.title.toLowerCase().includes(search.toLowerCase())
+    ) as page}
       <SubNavMenuItem>
-        <a href="general/installation">Installation</a>
+        <a href={`${page.group}/${page.title.replace(/ /g, '')}`.toLowerCase()}>
+          {page.group} &gt; {page.title}
+        </a>
       </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="general/styles">Styles</a>
-      </SubNavMenuItem>
-    </SubNavMenu>
-  </AccordionItem>
-  <AccordionItem header="LAYOUT">
-    <SubNavMenu type="vertical">
-      <SubNavMenuItem>
-        <a href="layout/page">Page</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="layout/container">Container</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="layout/grid">Grid</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="layout/level">Level</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="layout/panel">Panel</a>
-      </SubNavMenuItem>
-    </SubNavMenu>
-  </AccordionItem>
-  <AccordionItem header="COMPONENTS">
-    <SubNavMenu type="vertical">
-      <SubNavMenuItem>
-        <a href="components/accordion">Accordion</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/alert">Alert</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/autocomplete">Auto Complete</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/badge">Badge</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/breadcrumb">Breadcrumb</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/button">Button</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/calendar">Calendar</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/card">Card</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/colorpicker">Color Picker</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/coverimage">Cover Image</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/datepicker">Date Picker</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/dropdown">Drop Down</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/imagebutton">Image Button</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/notification">Notification</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/offscreen">Off Screen</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/palette">Palette</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/progress">Progress</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/slider">Slider</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/tabgroup">Tab Group</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/table">Table</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/tag">Tag</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="components/taginput">Tag Input</a>
-      </SubNavMenuItem>
-    </SubNavMenu>
-  </AccordionItem>
-  <AccordionItem header="NAVIGATION">
-    <SubNavMenu type="vertical">
-      <SubNavMenuItem>
-        <a href="nav/navbar">Nav Bar</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="nav/subnavmenu">Sub Nav Menu</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="nav/switcher">Switcher</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="nav/pagination">Pagination</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="nav/dotnav">Dot Navigation</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="nav/storynav">Story Navigation</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="nav/wizardnav">Wizard Navigation</a>
-      </SubNavMenuItem>
-    </SubNavMenu>
-  </AccordionItem>
-  <AccordionItem header="FORMS">
-    <SubNavMenu type="vertical">
-      <SubNavMenuItem>
-        <a href="forms/field">Field</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="forms/checkbox">CheckBox</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="forms/file">File</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="forms/number">Number</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="forms/radio">Radio</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="forms/select">Select</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="forms/textbox">TextBox</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="forms/textarea">TextArea</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="forms/validation">Validation</a>
-      </SubNavMenuItem>
-    </SubNavMenu>
-  </AccordionItem>
-  <AccordionItem header="DIALOGS">
-    <SubNavMenu type="vertical">
-      <SubNavMenuItem>
-        <a href="dialogs/info">Info Dialog</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="dialogs/confirm">Confirm Dialog</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="dialogs/prompt">Prompt Dialog</a>
-      </SubNavMenuItem>
-    </SubNavMenu>
-  </AccordionItem>
-  <AccordionItem header="CHARTS">
-    <SubNavMenu type="vertical">
-      <SubNavMenuItem>
-        <a href="charts/barchart">Bar Chart</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="charts/linechart">Line Chart</a>
-      </SubNavMenuItem>
-      <SubNavMenuItem>
-        <a href="charts/piechart">Pie Chart</a>
-      </SubNavMenuItem>
-    </SubNavMenu>
-  </AccordionItem>
-  <AccordionItem header="ICONS">
-    <SubNavMenu type="vertical">
-      <SubNavMenuItem>
-        <a href="components/icons">Icons</a>
-      </SubNavMenuItem>
-    </SubNavMenu>
-  </AccordionItem>
-</Accordion>
+    {/each}
+  </SubNavMenu>
+{:else}
+  <Accordion>
+    {#each groups as group}
+      <AccordionItem header={group.toUpperCase()}>
+        <SubNavMenu type="vertical">
+          {#each pages.filter(p => p.group === group) as page}
+            <SubNavMenuItem>
+              <a
+                href={`${page.group}/${page.title.replace(/ /g, '')}`.toLowerCase()}>
+                {page.title}
+              </a>
+            </SubNavMenuItem>
+          {/each}
+        </SubNavMenu>
+      </AccordionItem>
+    {/each}
+  </Accordion>
+{/if}
