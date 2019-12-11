06f54e1e14feb465c58ab2a429581a8967d8ab58
Andrew K <andrewjk@gmail.com>
2019-12-11 11:10:50 +1100

    Fix: basepath is no longer required

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
-		process.env.SAPPER_EXPORT && '/svelte-toolkit-docs' || '/',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
