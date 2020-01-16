e93e083e4071a31aa2489fe5770f8c4c872204bf
Andrew K <andrewjk@gmail.com>
2020-01-16 19:16:22 +1100

    Breaking: LineChart now requires a data property + has a few new features

--- src/routes/charts/linechart.svelte
+++ src/routes/charts/linechart.svelte
@@ -1,21 +1,97 @@
 <script>
-  import { LineChart, LineChartSeries } from "svelte-toolkit";
+  import { LineChart } from "svelte-toolkit";
 
-  let width = 400;
   let height = 200;
-  let value = 25;
-  let label = "Item 1";
+  let width = 400;
+
+  let xLabel = "Things";
+  let yLabel = "";
+  let labels = "Item1, Item2, Item3";
+  let data = "35, 25, 45";
   let color = "";
+  let data2 = "";
+  let color2 = "";
+  let showPoints = true;
+  let stepCount = 2;
+  let stepValue = 0;
+  let showXAxis = true;
+  let showYAxis = true;
+  let showHLines = false;
+  let showVLines = false;
 
-  $: exampleCode = `
-import { LineChart, LineChartSeries } from "svelte-toolkit";
+  $: labelsArray = labels.split(/[\s,;]/).filter(Boolean);
+  $: dataArray = data2 ? null : data.split(/[\s,;]/).filter(Boolean);
+  $: seriesArray = data2
+    ? [
+        { color, data: data.split(/[\s,;]/).filter(Boolean) },
+        { color: color2, data: data2.split(/[\s,;]/).filter(Boolean) }
+      ]
+    : null;
 
-<LineChart width={${width}} height={${height}}>
-  <LineChartSeries
-    values={[${value}, 35, 40]}
-    labels={['${label}', 'Item 2', 'Item 3']} />
-</LineChart>
+  $: exampleCode = data2
+    ? `
+import { LineChart } from "svelte-toolkit";
+
+<LineChart
+  width={${width}}
+  height={${height}} 
+  xLabel="${xLabel}"
+  yLabel="${yLabel}"
+  labels={[${labelsArray.map(l => `"${l}"`)}]}
+  series={[${seriesArray.map(
+    s => `
+    {
+      name: "",
+      color: "${s.color}",
+      data: [${s.data}]
+    }`
+  )}
+  ]}
+  showPoints={${showPoints}}
+  stepCount={${stepCount}}
+  stepValue={${stepValue}}
+  showXAxis={${showXAxis}}
+  showYAxis={${showYAxis}}
+  showHLines={${showHLines}}
+  showVLines={${showVLines}} />
+`.trim()
+    : `
+import { LineChart } from "svelte-toolkit";
+
+<LineChart
+  width={${width}}
+  height={${height}} 
+  xLabel="${xLabel}"
+  yLabel="${yLabel}"
+  labels={[${labelsArray.map(l => `"${l}"`)}]}
+  data={[${dataArray}]}
+  color="${color}"
+  showPoints={${showPoints}}
+  stepCount={${stepCount}}
+  stepValue={${stepValue}}
+  showXAxis={${showXAxis}}
+  showYAxis={${showYAxis}}
+  showHLines={${showHLines}}
+  showVLines={${showVLines}} />
 `.trim();
+
+  function addRandom() {
+    const darray = data.split(/[\s,;]/).filter(Boolean);
+    darray.push(Math.floor(Math.random() * 100) + 1);
+    data = darray.join(", ");
+    if (data2) {
+      const d2array = data2.split(/[\s,;]/).filter(Boolean);
+      while (d2array.length < darray.length) {
+        d2array.push(Math.floor(Math.random() * 100) + 1);
+      }
+      data2 = d2array.join(", ");
+    }
+    const larray = labels.split(/[\s,;]/).filter(Boolean);
+    while (larray.length < darray.length) {
+      larray.push(`Item${larray.length}`);
+    }
+    labels = larray.join(", ");
+  }
 </script>
 
 <svelte:head>
@@ -24,17 +100,26 @@ import { LineChart, LineChartSeries } from "svelte-toolkit";
 
 <div class="container">
   <h1>Line Chart</h1>
-  <p>A simple SVG line chart for displaying data.</p>
+  <p>A simple SVG bar chart for displaying data.</p>
 
   <h2>Demo</h2>
   <div class="block">
-    <LineChart {width} {height}>
-      <LineChartSeries
-        values={[value, 35, 40]}
-        labels={[label, 'Item 2', 'Item 3']} />
-      <LineChartSeries values={[50, 25, 32]} />
-      <LineChartSeries values={[18, 66, 23]} />
-    </LineChart>
+    <LineChart
+      {height}
+      {width}
+      {xLabel}
+      {yLabel}
+      labels={labelsArray}
+      data={dataArray}
+      {color}
+      series={seriesArray}
+      {showPoints}
+      {stepCount}
+      {stepValue}
+      {showXAxis}
+      {showYAxis}
+      {showHLines}
+      {showVLines} />
   </div>
 
   <h2>Properties</h2>
@@ -46,17 +131,10 @@ import { LineChart, LineChartSeries } from "svelte-toolkit";
           <th>Default</th>
           <th>Description</th>
           <th>Change</th>
+          <th />
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>width</td>
-          <td>400</td>
-          <td>The width of the chart, in pixels</td>
-          <td>
-            <input type="number" bind:value={width} />
-          </td>
-        </tr>
         <tr>
           <td>height</td>
           <td>200</td>
@@ -64,30 +142,162 @@ import { LineChart, LineChartSeries } from "svelte-toolkit";
           <td>
             <input type="number" bind:value={height} />
           </td>
+          <td />
         </tr>
         <tr>
-          <td>LineChartSeries: value</td>
-          <td>[]</td>
-          <td>The values of the items</td>
+          <td>width</td>
+          <td>400</td>
+          <td>The width of the chart, in pixels. Set to 0 to auto-size</td>
           <td>
-            <input type="number" bind:value />
+            <input type="number" bind:value={width} />
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>xLabel</td>
+          <td />
+          <td>A label to display along the x-axis</td>
+          <td>
+            <input type="text" bind:value={xLabel} />
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>yLabel</td>
+          <td />
+          <td>A label to display along the y-axis</td>
+          <td>
+            <input type="text" bind:value={yLabel} />
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>labels</td>
+          <td />
+          <td>The labels to display for each bar</td>
+          <td>
+            <input type="text" bind:value={labels} />
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>data</td>
+          <td />
+          <td>The data to display as bars</td>
+          <td>
+            <input type="text" bind:value={data} />
+          </td>
+          <td>
+            <button on:click={addRandom}>++</button>
           </td>
         </tr>
         <tr>
-          <td>LineChartSeries: label</td>
-          <td>[]</td>
-          <td>The labels of the items to show along bottom of the chart</td>
-          <td>
-            <input type="text" bind:value={label} />
-          </td>
-        </tr>
-        <tr>
-          <td>LineChartSeries: color</td>
-          <td>A default color, chosen from a list</td>
-          <td>The color of the series</td>
+          <td>color</td>
+          <td />
+          <td>The color of the bars</td>
           <td>
             <input type="text" bind:value={color} />
           </td>
+          <td />
+        </tr>
+        <tr>
+          <td>data 2</td>
+          <td />
+          <td>You can pass multiple data arrays in a series property</td>
+          <td>
+            <input type="text" bind:value={data2} />
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>color 2</td>
+          <td />
+          <td>You can pass multiple colors in a series property</td>
+          <td>
+            <input type="text" bind:value={color2} />
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>showPoints</td>
+          <td>true</td>
+          <td>set to true to show points on the line, or false to hide them</td>
+          <td>
+            <label>
+              <input type="checkbox" bind:checked={showPoints} />
+              Show points
+            </label>
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>stepCount</td>
+          <td>2</td>
+          <td>The number of steps to show on the y-axis</td>
+          <td>
+            <input type="number" bind:value={stepCount} />
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>stepValue</td>
+          <td />
+          <td>
+            The value of each step on the y-axis. By default, this is
+            automatically calculated
+          </td>
+          <td>
+            <input type="number" bind:value={stepValue} />
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>showXAxis</td>
+          <td>true</td>
+          <td>set to true to show the x-axis line, or false to hide it</td>
+          <td>
+            <label>
+              <input type="checkbox" bind:checked={showXAxis} />
+              Show the x-axis
+            </label>
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>showYAxis</td>
+          <td>true</td>
+          <td>set to true to show the y-axis line, or false to hide it</td>
+          <td>
+            <label>
+              <input type="checkbox" bind:checked={showYAxis} />
+              Show the y-axis
+            </label>
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>showHLines</td>
+          <td>false</td>
+          <td>set to true to show horizontal gridlines</td>
+          <td>
+            <label>
+              <input type="checkbox" bind:checked={showHLines} />
+              Show horizontal gridlines
+            </label>
+          </td>
+          <td />
+        </tr>
+        <tr>
+          <td>showVLines</td>
+          <td>false</td>
+          <td>set to true to show vertical gridlines</td>
+          <td>
+            <label>
+              <input type="checkbox" bind:checked={showVLines} />
+              Show vertical gridlines
+            </label>
+          </td>
+          <td />
         </tr>
       </tbody>
     </table>
@@ -95,6 +305,6 @@ import { LineChart, LineChartSeries } from "svelte-toolkit";
 
   <h2>Code</h2>
   <div class="block">
-    <pre>{exampleCode} </pre>
+    <pre>{exampleCode}</pre>
   </div>
 </div>
