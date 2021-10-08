c3bf2ab1988892c7cfecfa14a3524ce2a40f3322
Andrew K <andrewjk@gmail.com>
2021-10-08 14:59:21 +1100

    Chore: update dependencies, remove unnecessary config

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,8 +9,6 @@
     Container,
     Grid,
     GridItem,
-    loadTheme,
-    getTheme
   } from "svelte-toolkit";
 
   import Header from "$lib/Header.svelte";
@@ -18,54 +16,7 @@
   import Sidebar from "$lib/Sidebar.svelte";
 
   export let segment;
-
-  /*
-  onMount(() => {
-    loadTheme({
-      "--header-back-color": "aliceblue",
-      "--footer-back-color": "slategray",
-      "--body-back-color": "#555",
-      "--body-text-color": "white",
-      "--info-color": "pink",
-      "--link-color": "goldenrod",
-      "--label-text-color": "#ccc"
-    });
-
-    console.log(getTheme());
-  });
-  */
 </script>
-
-<style lang="scss">
-  //:root {
-  //  --info-color: green;
-  //}
-
-  :global(.outer-panel) {
-    background-color: #fafafa;
-    border: 1px dashed #d2d2d2;
-    padding: 2rem;
-    width: 100%;
-  }
-
-  :global(.inner-panel) {
-    background-color: #fff;
-    border: 1px solid #d2d2d2;
-    padding: 2rem;
-    text-align: center;
-    width: 100%;
-  }
-
-  :global(.title) {
-    font-size: 3rem;
-  }
-
-  :global(.number) {
-    color: darkgray;
-    font-size: 9.6rem;
-    text-align: center;
-  }
-</style>
 
 <Toolkit />
 
@@ -97,3 +48,30 @@
     <Footer {segment} />
   </PageFooter>
 </Page>
+
+<style>
+  :global(.outer-panel) {
+    background-color: #fafafa;
+    border: 1px dashed #d2d2d2;
+    padding: 2rem;
+    width: 100%;
+  }
+
+  :global(.inner-panel) {
+    background-color: #fff;
+    border: 1px solid #d2d2d2;
+    padding: 2rem;
+    text-align: center;
+    width: 100%;
+  }
+
+  :global(.title) {
+    font-size: 3rem;
+  }
+
+  :global(.number) {
+    color: darkgray;
+    font-size: 9.6rem;
+    text-align: center;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,21 @@
-import staticAdapter from '@sveltejs/adapter-static';
-import sveltePreprocess from 'svelte-preprocess';
+import staticAdapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // Use the static adapter so that we can deploy to GitHub Pages
     adapter: staticAdapter({
-      pages: 'docs',
-      assets: 'docs',
-      fallback: null
+      pages: "docs",
+      assets: "docs",
+      fallback: null,
     }),
     // GitHub Pages stores the website at /svelte-toolkit-docs
     // We need to comment this out if running in dev where it doesn't apply
     paths: {
-      base: '/svelte-toolkit-docs',
-      assets: '/svelte-toolkit-docs',
+      base: "/svelte-toolkit-docs",
+      assets: "/svelte-toolkit-docs",
     },
-    vite: (() => ({
-      resolve: {
-        // Add .svelte to the default extensions to resolve svelte files in node_modules
-        // Without this, the svelte-toolkit components wouldn't be resolved
-        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.svelte']
-      }
-    }))
   },
-  // Preprocess SASS styles in Svelte components
-  preprocess: sveltePreprocess()
 };
 
 export default config;
